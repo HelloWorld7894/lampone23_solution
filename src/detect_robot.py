13242d8e17ed49fe15729b705b829ab2316ac407
salivo <andrijsalivon@gmail.com>
2023-08-28 14:56:58 +0200

    robot_detection

--- src/detect_robot.py
+++ src/detect_robot.py
@@ -1,8 +1,16 @@
-#other imports
 import cv2
-import skimage
-import numpy as np
-import matplotlib.pyplot as plt
+import cv2.aruco as aruco
 
-def main():
-    pass
+image = cv2.imread('image.png')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+parameters = aruco.DetectorParameters()
+
+corners, ids, rejected = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+
+image_with_markers = aruco.drawDetectedMarkers(image, corners, ids)
+
+cv2.imshow('Detected Markers', image_with_markers)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
