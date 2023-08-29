ef6e1fe18b19f9b2a66000605f55e6aa8926bdb4
salivo <andrijsalivon@gmail.com>
2023-08-29 10:45:33 +0200

    just remove some lines

--- src/detect_robot.py
+++ src/detect_robot.py
@@ -6,6 +6,23 @@ import matplotlib.pyplot as plt
 
 def main(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+    parameters = cv2.aruco.DetectorParameters()
+    detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
+    corners, ids, rejected = detector.detectMarkers(gray)
+    return corners
+
+
+
+
+
+
+
+
+# just test to see
+if __name__ == "__main__":
+    image = cv2.imread('assets/image.png')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
     parameters = cv2.aruco.DetectorParameters()
@@ -13,15 +30,8 @@ def main(image):
     detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
 
     corners, ids, rejected = detector.detectMarkers(gray)
-    print(corners)
-    print(ids)
-
-    #return cv2.aruco.drawDetectedMarkers(image, corners, ids) # TODO:  return just angle and rentangle 2 points 
-    return corners
-
-if __name__ == "__main__":
-    image = cv2.imread('assets/image.png')
-    image_with_markers = main(image)
+    
+    image_with_markers = cv2.aruco.drawDetectedMarkers(image, corners, ids)
     cv2.imshow('Detected Markers', image_with_markers)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
