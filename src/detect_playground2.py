d967b294bc270861da3e68669499726426df7e2e
HelloWorld7894 <danpojhan@protonmail.com>
2023-08-29 10:13:08 +0200

    addedautocrop

--- src/detect_playground2.py
+++ src/detect_playground2.py
@@ -14,42 +14,31 @@ def main(empty_image):
 
 
     # resize img
-    cropped_empty_img = empty_image[275:740, 675:1300]
+    #cropped_empty_img = empty_image[275:740, 675:1300]
 
 
-    #simpling img
-
-    imgray = cv.cvtColor(cropped_empty_img, cv.COLOR_BGR2GRAY)
+    imgray = cv.cvtColor(empty_image, cv.COLOR_BGR2GRAY)
     ret, thresh = cv.threshold(imgray, 127, 255, 0)
 
     #geting contours
     contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-    # Sorting
+    if len(contours) != 0:
+        c = max(contours, key = cv.contourArea)
+        x, y, w, h = cv.boundingRect(c)
+        margin = 35
 
-    Ysort = []
-    Xsort = []
-    SortedDots = []
-    # Y sort
-    for countor in contours[2:]:
-        cordX = []
-        cordY = []
-        for dot in countor:
-            cordX.append(dot[0][0])
-            cordY.append(dot[0][1])
-            
-        Ysort.append([[min(cordX),min(cordY)],[max(cordX),max(cordY)]])
-    Ysort.sort(key=lambda x: (x[0][1])) 
-    # X sort and creating 8x8x2x2 4D aray lol this code is fucked shit but it sorting
-    for i in range(8):
-        if i == 0:
-            Xsort = Ysort[:8]
-        else:
-            Xsort = Ysort[i*8:i*8+8]
-        Xsort.sort(key=lambda x: (x[0][0]))
-        SortedDots.append(Xsort)
-    return SortedDots
+        X1 = x + margin
+        Y1 = y + margin
 
+        X2 = x + w - margin
+        Y2 = y + h - margin
+
+        # draw the biggest contour (c) in green
+        cv.rectangle(empty_image,(X1, Y1), (X2, Y2), (0, 255, 0), 2)
+
+    cv.imshow("img", empty_image)
+    cv.waitKey(0)
 
 
 if __name__ == "__main__":
--- src/load_frame.py
+++ src/load_frame.py
@@ -1,9 +1,31 @@
 #other imports
-import cv2
+import cv2 as cv
 import skimage
 import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
     URL = "http://192.168.100.22/image/image.png"
-    return skimage.io.imread(URL, as_gray=False)
+    image = skimage.io.imread(URL, as_gray=False)
+
+    imgray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    ret, thresh = cv.threshold(imgray, 127, 255, 0)
+
+    #geting contours
+    contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    if len(contours) != 0:
+        c = max(contours, key = cv.contourArea)
+        x, y, w, h = cv.boundingRect(c)
+        margin = 35
+
+        X1 = x + margin
+        Y1 = y + margin
+
+        X2 = x + w - margin
+        Y2 = y + h - margin
+
+        return image[Y1:Y2, X1:X2]
+    else:
+        print("problem with finding contours, exiting program")
+        exit(1)
