e3ae9b905ff0d971eb3e46cfbe747efaeebfe178
frantisekhrimal <143405775+frantisekhrimal@users.noreply.github.com>
2023-08-29 10:00:22 +0200

    Update send_solution.py

--- src/send_solution.py
+++ src/send_solution.py
@@ -1,14 +1,17 @@
 import socket
 
-def main(path):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host ="142.168.109.22"
-    port =9999
-    s.connect((host,port))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host ="142.168.109.22"
+port =9999
+s.connect((host,port))
 
-    s.send('e'.encode()) 
-    data = ''
-    data = s.recv(1024).decode()
-    print (data)
+def ts(str):
+   s.send('e'.encode()) 
+   data = ''
+   data = s.recv(1024).decode()
+   print (data)
 
-    s.close()
+ts(s)
+
+s.close ()
+
