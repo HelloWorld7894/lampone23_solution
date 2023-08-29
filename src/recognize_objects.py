aa7af0f7a493fc78acc9927637aed94aa776273d
Sles Usagi <usagisles@mail.com>
2023-08-29 10:16:22 +0200

    lgbt lable good but terible

--- src/recognize_objects.py
+++ src/recognize_objects.py
@@ -5,12 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 from skimage.filters import threshold_otsu
+import scipy.signal
+
 
 def main(URL):
-
-    
-
-    # print(img_crop.shape)
 
     fig = plt.figure(figsize=(10, 7))
     rows = 3
@@ -40,7 +38,6 @@ def main(URL):
 
     fig.add_subplot(rows, columns, 4)
 
-    #img = img_color[0]  ^ img_color[2]
     img_xor = (np.logical_xor(img_color[0], img_color[2]))
 
     plt.imshow(img_xor, cmap="gray")
@@ -50,7 +47,6 @@ def main(URL):
 
     fig.add_subplot(rows, columns, 5)
 
-    #img = img_color[0]  ^ img_color[2]
     img_and = (np.logical_and(img_color[0], img_color[2]))
 
     plt.imshow(img_and, cmap="gray")
@@ -60,18 +56,39 @@ def main(URL):
 
     fig.add_subplot(rows, columns, 6)
 
-    #img = img_color[0]  ^ img_color[2]
     img = (np.logical_xor(img_color[0], img_and))
+    kernel = np.ones((10,10))
+    img = scipy.signal.convolve2d(img,kernel,boundary='symm')
+    img = img > 15
+    plt.imshow(img,cmap = "gray")
 
     plt.imshow(img, cmap="gray")
     plt.axis('off')
     plt.title("final")
-
+    print(img)
     plt.show()
+
+
+    # Find connected components in the binary image
+    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(img)
+
+    # Create a random color map for visualization
+    color_map = np.random.randint(0, 255, size=(num_labels, 3), dtype=np.uint8)
+
+    # Set background label to black (optional)
+    color_map[0] = [0, 0, 0]
+
+    # Create a colored label image
+    colored_labels = color_map[labels]
+
+    # Display the original image and the labeled image
+    cv2.imshow('Original Image', img)
+    cv2.imshow('Labeled Image', colored_labels)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
     pass
 
-main("assets/image.png")
+if __name__ == "__main__":
+    main("assets/image.png")
 
-#x630 y250
-#x1333 y760
