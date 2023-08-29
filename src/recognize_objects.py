aa645aeddf1978af3c8eee0693245931d33711cf
Sles Usagi <usagisles@mail.com>
2023-08-30 00:08:40 +0200

    green

--- src/recognize_objects.py
+++ src/recognize_objects.py
@@ -39,11 +39,11 @@ def main(img, debug = False):
     imgs.append(img_clear)
     imgs_titles.append(f"final {img_clear.shape}")
 
-    img_xor = (np.logical_xor(imgs[1], imgs[2]))
-    img_xor = (np.logical_xor(img_xor, img_and))
-    img_xor = (np.logical_xor(img_xor, imgs[1]))
-    imgs.append(img_xor)
-    imgs_titles.append(f"and 1 2 {img_xor.shape}")
+    img_and12 = (np.logical_xor(imgs[1], imgs[2]))
+    img_and12 = (np.logical_xor(img_and12, img_and))
+    img_and12 = (np.logical_xor(img_and12, imgs[1]))
+    imgs.append(img_and12)
+    imgs_titles.append(f"and 1 2 {img_and12.shape}")
 
     img_xor = (np.logical_xor(imgs[0], imgs[2]))
     kernel = np.ones((10,10))
@@ -62,25 +62,31 @@ def main(img, debug = False):
     imgs.append(img_red)
     imgs_titles.append(f"kulatý {img_red.shape}")
 
+    img_green = (np.logical_and(imgs[0], imgs[1]))
+    img_green = (np.logical_xor(img_green, imgs[2]))
+    img_green = (np.logical_xor(img_green, img_and12))
+    kernel = np.ones((40,40))
+    img_green = scipy.signal.convolve2d(img_green,kernel,boundary='symm')
+    img_green = img_green > 400
+    imgs.append(img_green)
+    imgs_titles.append(f"green {img_green.shape}")
+
     if debug:
         fig = plt.figure(figsize=(10, 7))
-        for i in range(1,11):
+        for i in range(1,12):
             fig.add_subplot(rows, columns, i)
             plt.imshow(imgs[i-1], cmap="gray")
             plt.axis('off')
             plt.title(f"{imgs_titles[i-1]}")
         plt.show()
+    
     """labeling"""
-    insert = [img_clear,img_red]
+    insert = [img_clear,img_red,img_green]
     output = []
 
     for binary_array in insert:
-
-
         num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(binary_array.astype(np.uint8))
-
         labels = []
-
         for label in range(1, num_labels):
             leftmost = stats[label, cv2.CC_STAT_LEFT]
             topmost = stats[label, cv2.CC_STAT_TOP]
@@ -89,19 +95,12 @@ def main(img, debug = False):
             area = stats[label, cv2.CC_STAT_AREA]
             centroid_x, centroid_y = centroids[label]
             labels.append([leftmost,topmost,width,height,centroid_x,centroid_y])
-            #print(f"Label {label}: Area={area}, Bounding Box=({leftmost}, {topmost}, {width}, {height}), Centroid=({centroid_x}, {centroid_y})")
-
+            print(f"Label {label}: Area={area}, Bounding Box=({leftmost}, {topmost}, {width}, {height}), Centroid=({centroid_x}, {centroid_y})")
         output.append(labels)
-
     #print(output)
-    
-    plt.show()
-
-
 
     return output
 
-
 if __name__ == "__main__":
-    main(skimage.io.imread("assets/image.png", as_gray=False)[250:760,650:1333,:])
+    main(skimage.io.imread("assets/imagewgreen.png", as_gray=False)[270:760,650:1333,:],True)
 
