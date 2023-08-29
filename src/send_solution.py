5e787d69c6f2c530049c6b52629a2db971cf93c4
HelloWorld7894 <danpojhan@protonmail.com>
2023-08-29 09:51:38 +0200

    send_solution reformat

--- src/send_solution.py
+++ src/send_solution.py
@@ -1,18 +1,14 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host ="142.168.109.22"
-port =9999
-s.connect((host,port))
-
-def ts(str):
-   s.send('e'.encode()) 
-   data = ''
-   data = s.recv(1024).decode()
-   print (data)
-
-s.close ()
-
-
 def main(path):
-    pass
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host ="142.168.109.22"
+    port =9999
+    s.connect((host,port))
+
+    s.send('e'.encode()) 
+    data = ''
+    data = s.recv(1024).decode()
+    print (data)
+
+    s.close()
