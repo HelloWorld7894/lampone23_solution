bb99ff1ee104dc8e6f65193fc8c3088944995c27
salivo <andrijsalivon@gmail.com>
2023-08-29 11:09:47 +0200

    ok

--- src/main.py
+++ src/main.py
@@ -8,14 +8,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def solve():
-    img = load_frame.main("./assets/image_empty.png")
-    #img = load_frame.main()
+    img = load_frame.main()
+    playground = detect_playground.main(load_frame.main("./assets/image_empty.png"))
     robot = detect_robot.main(img)
-    playground = detect_playground.main(img)
     objects = recognize_objects.main(img)
     array = analyze_playground.main(playground, robot, None)
     path = generate_path.main(array)
-    #send_solution.main(path)
+    send_solution.main(path)
 
 
 if __name__ == "__main__":
