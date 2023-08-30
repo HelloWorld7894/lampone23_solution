b015503d5825cf56c4b7472c9eab6f7f24f10aad
salivo <andrijsalivon@gmail.com>
2023-08-30 13:26:44 +0200

    good boy

--- src/main.py
+++ src/main.py
@@ -29,23 +29,20 @@ def solve():
     #some logging stuff
     global logging
     args = parser.parse_args()
-    print(args.v)
 
     if args.v:
         logging = True
 
-    img = load_frame.main(PATH + "/assets/image_all.png")
-    img_log = img.copy()
-
-    playground = detect_playground.main(img, logging)
+    img = load_frame.main()
+    empty_image = load_frame.main("assets/image_empty.png")
+    playground = detect_playground.main(empty_image, logging)
     robot = detect_robot.main(img, logging)
     objects = recognize_objects.main(img, logging)
     array = analyze_playground.main(playground, robot, objects, logging)
     path = generate_path.main(array, logging)
-    
+    send_solution.main(path)
     cv2.imshow("logging", img)
     cv2.waitKey(0)
-    #send_solution.main(path)
 
 
 
