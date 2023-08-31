01ff59cfd83ddb8ba48bf8f27b5d53c4822495d9
HelloWorld7894 <danpojhan@protonmail.com>
2023-08-31 11:21:48 +0200

    command conversion works

--- src/translate_path.py
+++ src/translate_path.py
@@ -7,10 +7,12 @@ import os
 from log import log_true, log_false, log_warn
 
 def main(heading, path_array, verbose = False):
+    print(path_array)
 
-    
     curr_heading = heading
+    curr_pos = path_array[0]
     out_string = ""
+    append = ""
     for i, curr_coord in enumerate(path_array):
         if i + 1 == len(path_array):
             break
@@ -18,13 +20,21 @@ def main(heading, path_array, verbose = False):
         d_y = curr_coord[0] - path_array[i+1][0] 
         d_x = curr_coord[1] - path_array[i+1][1]
 
+        if (curr_pos[0] == curr_coord[0] and curr_pos[1] == curr_coord[1]):
+            #robot just needs to rotate
+            append = ""
+        else:
+            append = "F"
+
         if d_y == 1: #up
             if curr_heading == "N":
                 out_string += "F"
             elif curr_heading == "W":
-                out_string += "R"
+                out_string += append + "R"
             elif curr_heading == "E":
-                out_string += "L"
+                out_string += append + "L"
+            elif curr_heading == "S":
+                out_string += append + "LL"
 
             curr_heading = "N"
 
@@ -32,35 +42,48 @@ def main(heading, path_array, verbose = False):
             if curr_heading == "S":
                 out_string += "F"
             elif curr_heading == "W":
-                out_string += "L"
+                out_string += append + "L"
             elif curr_heading == "E":
-                out_string += "R"
+                out_string += append + "R"
+            elif curr_heading == "N":
+                out_string += append + "LL"
 
             curr_heading = "S"
 
         if d_x == 1: #left
             if curr_heading == "N":
-                out_string += "L"
+                out_string += append + "L"
             elif curr_heading == "W":
                 out_string += "F"
             elif curr_heading == "S":
-                out_string += "R"
+                out_string += append + "R"
+            elif curr_heading == "E":
+                out_string += append + "LL"
 
             curr_heading = "W"
 
         elif d_x == -1: #right
             if curr_heading == "N":
-                out_string += "R"
+                out_string += append + "R"
             elif curr_heading == "E":
                 out_string += "F"
             elif curr_heading == "S":
-                out_string += "L"
+                out_string += append + "L"
+            elif curr_heading == "W":
+                out_string += append + "LL"
 
             curr_heading = "E"
+        
+        curr_pos = curr_coord
+
+        if i == 0:
+            #we need to delete the first command because of the shift
+            out_string = out_string[1:]
 
     if verbose:
         log_true("generated robot commands")
         print("commands: " + out_string)
+        
     return out_string
 
 if __name__ == "__main__":
