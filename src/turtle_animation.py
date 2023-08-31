6a3c98d12f6ef36014f92307316100f3dd67435c
salivo <andrijsalivon@gmail.com>
2023-08-31 13:55:58 +0200

    3x2

--- src/turtle_animation.py
+++ src/turtle_animation.py
@@ -1,9 +1,23 @@
 import turtle
 import time
+import urllib.request
 
-def animate_path(img): 
-    tr = turtle.Turtle()
+
+
+def animate_path(path): 
+    image_url = "http://192.168.100.22/image/image.png"
+    local_filename, headers = urllib.request.urlretrieve(image_url, "assets/image_last.png")
+    t = turtle.Turtle()
     wn = turtle.Screen()
-    wn.setup(width=500,height=500)
-    wn.bgpic(img)
+    wn.setup(width=700,height=550)
+    wn.bgpic("assets/image_last.png")
+    t.penup()
+    t.forward(-10)
+    t.left(90)
+    t.forward(70)
+    for i in path
     wn.mainloop()
+    
+
+
+animate_path()
