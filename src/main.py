6abb2c7422c425b348ab662c012872088a357dad
salivo <andrijsalivon@gmail.com>
2023-08-31 10:22:50 +0200

    perfect visuanal

--- src/main.py
+++ src/main.py
@@ -13,6 +13,7 @@ import argparse
 import os
 from log import log_true, log_false, log_warn
 import json
+from visualization import visualization as visual
 
 from matplotlib.colors import ListedColormap
 import matplotlib
@@ -46,50 +47,29 @@ else:
 def solve():
     #some logging stuff
     global logging
-    global noGUI
     args = parser.parse_args()
     
     if args.v:
         logging = True
         print("Logging set to true")
 
-    fig, axs = plt.subplots(2, 3, figsize=(15, 10))
+    
 
     img = load_frame.main(json_data["resource"], verbose=logging)
     empty_image = load_frame.main(PATH + "/assets/image_empty.png")
-    
-    axs[0][0].imshow(img)
-    axs[0][0].set_title('orichinal')
-    
+
     playground, detect_playground_img = detect_playground.main(empty_image, logging)
-    axs[0][1].imshow(detect_playground_img)
-    axs[0][1].set_title('playground_detection')
-    
-    
     img_for_robot = img.copy()
     robot, robotImg = detect_robot.main(img_for_robot, logging)
-    axs[0][2].imshow(robotImg)
-    axs[0][2].set_title('robot_detection')
-    
     objects, objects_img = recognize_objects.main(img, logging)
-    axs[1][0].imshow(objects_img)
-    axs[1][0].set_title('Recognize_objects')
-    
     array = analyze_playground.main(playground, robot, objects, logging)
-    #visualization of 2d np aray
-    colors = ['white', 'black', 'green', 'blue', 'red','orange']
-    cmap = ListedColormap(colors)
-    axs[1][1].imshow(array, cmap=cmap)
-    axs[1][1].set_title('8x8 Array')
-    
-    if logging:
-        plt.tight_layout()
-        plt.show()
-
+        
     path = generate_path.main(array, logging)
+    
     if send:
         send_solution.main(path)
-
+    if True:
+        visual(img,detect_playground_img,robotImg,objects_img,array,path)
 
 if __name__ == "__main__":
     solve()
